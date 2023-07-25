a8a836a3747f64ddd3cf3e2f39aae13fc9d945c6
lemiltock <lemiltock@gmail.com>
2023-07-26 06:35:30 +1000

    Renamed to Proof of Concept and added new test file. Then stated work on one sample correction

--- examples/agent-based-models/poc-sir-test.jl
+++ examples/agent-based-models/poc-sir-test.jl
@@ -6,21 +6,74 @@ using Turing
 using DataFrames
 using StatsPlots
 
+function sir_ode!(du,u,p,t)
+    (S,I,R,C) = u
+    (β,c,γ) = p
+    N = S+I+R
+    infection = β*c*I/N*S
+    recovery = γ*I
+    @inbounds begin
+        du[1] = -infection
+        du[2] = infection - recovery
+        du[3] = recovery
+        du[4] = infection
+    end
+nothing
+end;
+tmax = 40.0
+tspan = (0.0,tmax)
+obstimes = 1.0:1.0:tmax
+u0 = [990.0,10.0,0.0,0.0] # S,I.R,C
+p = [0.05,10.0,0.25]; # β,c,γ
+
+prob_ode = ODEProblem(sir_ode!,u0,tspan,p);
+sol_ode = solve(prob_ode,
+                Tsit5(),
+                saveat = 1.0);
+StatsPlots.plot(sol_ode)
+
+# Generate data from ODE above.
+C = Array(sol_ode)[4,:] # Cumulative cases
+X = C[2:end] - C[1:(end-1)];
+Random.seed!(1234)
+Y = rand.(Poisson.(X));
+StatsPlots.bar(obstimes,Y,legend=false)
+StatsPlots.plot!(obstimes,X,legend=false)
+
+@model bayes_sir(y) = begin
+  # Calculate number of timepoints
+  l = length(y)
+  i₀ ~ Uniform(0.0,1.0)
+  β ~ Uniform(0.0,1.0)
+  I = i₀*1000.0
+  u0=[1000.0-I,I,0.0,0.0]
+  p=[β,10.0,0.25]
+  tspan = (0.0,float(l))
+  prob = ODEProblem(sir_ode!,
+          u0,
+          tspan,
+          p)
+  sol = solve(prob,
+              Tsit5(),
+              saveat = 1.0)
+  sol_C = Array(sol)[4,:] # Cumulative cases
+  sol_X = sol_C[2:end] - sol_C[1:(end-1)]
+  l = length(y)
+  for i in 1:l
+    y[i] ~ Poisson(sol_X[i])
+  end
+end;
+
+ode_nuts = sample(bayes_sir(Y),NUTS(1.0),10000);
+
+describe(ode_nuts)
+StatsPlots.plot(ode_nuts)
+
+# Agent based model version (Continuous for now, use pred/prey for discreet)
 using Agents, Random
 using InteractiveDynamics
 using CairoMakie
 
-# Approximate/true model settings
-N_samples = 1000
-n_adapt = 1000
-
-# Optimisation settings
-N_importance = 200
-N_energy = 1000
-energyβ = 1.0
-vmultiplier = 2.0
-
-# Generate data with ABM
 const steps_per_day = 24
 
 @agent PoorSoul ContinuousAgent{2} begin
@@ -73,6 +126,14 @@ function sir_initiation(;
     return model
 end
 
+# Makes a picture of initial state
+#sir_model = sir_initiation()
+
+#sir_colours(a) = a.status == :S ? "#2b2b33" : a.status == :I ? "#bf2642" : "#338c54"
+
+#fig, ax, abmp = abmplot(sir_model; ac = sir_colours)
+#fig
+
 function transmit!(a1, a2, rp)
     # Need only 1 infected to transmit
     count(a.status == :I for a in (a1, a2)) ≠ 1  && return
@@ -121,79 +182,31 @@ recovered(x) = count(i == :R for i in x)
 adata = [(:status, infected), (:status, recovered)]
 
 data1, _ = run!(sir_model, sir_agent_step!,  sir_model_step!, 40*24; adata)
+# Make data Z
 
-# Generate data for ODE
 tmp = data1[1:24:end, 2:3] # Grab one obs per day
 infect = tmp[2:end,1] - tmp[1:(end-1),1]
 recov = tmp[2:end,2] - tmp[1:(end-1),2]
-Y = infect + recov
+Z = infect + recov
 
-# Setup SIR ODE
-function sir_ode!(du,u,p,t)
-    (S,I,R,C) = u
-    (β,c,γ) = p
-    N = S+I+R
-    infection = β*c*I/N*S
-    recovery = γ*I
-    @inbounds begin
-        du[1] = -infection
-        du[2] = infection - recovery
-        du[3] = recovery
-        du[4] = infection
-    end
-nothing
-end;
+figure = Figure()
+ax = figure[1, 1] = Axis(figure; ylabel = "Individuals")
+l1 = lines!(ax, data1[:, dataname((:status, infected))], colour = "#bf2642")
+l2 = lines!(ax, data1[:, dataname((:status, recovered))], colour = "#338c54")
+figure[1, 2][1,1] =
+    Legend(figure, [l1, l2], ["Infected", "Recovered"])
+figure
 
-@model bayes_sir(y) = begin
-  # Calculate number of timepoints
-  l = length(y)
-  i₀ ~ Uniform(0.0,1.0)
-  β ~ Uniform(0.0,1.0)
-  I = i₀*1000.0
-  u0=[1000.0-I,I,0.0,0.0]
-  p=[β,10.0,0.25]
-  tspan = (0.0,float(l))
-  prob = ODEProblem(sir_ode!,
-          u0,
-          tspan,
-          p)
-  sol = solve(prob,
-              Tsit5(),
-              saveat = 1.0)
-  sol_C = Array(sol)[4,:] # Cumulative cases
-  sol_X = sol_C[2:end] - sol_C[1:(end-1)]
-  l = length(y)
-  for i in 1:l
-    y[i] ~ Poisson(sol_X[i])
-  end
-end;
-
-ode_nuts = sample(bayes_sir(Y),NUTS(1.0),1000);
-
-#describe(ode_nuts)
-#StatsPlots.plot(ode_nuts)
-
-# Get posterior samples
-post_i₀ = ode_nuts[:i₀]
-post_β = ode_nuts[:β]
-
-# Setup for approximate distribution (for first dist)
-tmax = 40.0
-tspan = (0.0,tmax)
-obstimes = 1.0:1.0:tmax
-I = post_i₀[1]*1000.0
-u0 = [1000.0 - I,I,0.0,0.0] # S,I.R,C
-p = [post_β[1],10.0,0.25]; # β,c,γ
-
-prob_ode = ODEProblem(sir_ode!,u0,tspan,p); sol_ode = solve(prob_ode,
-                Tsit5(),
-                saveat = 1.0);
-sol_ode = solve(prob_ode,
-                Tsit5(),
-                saveat = 1.0);
-
-# Generate new data from ODE above
-C = Array(sol_ode)[4,:] # Cumulative cases
-X = C[2:end] - C[1:(end-1)];
-Z = rand.(Poisson.(X))
-tmp_ode_nuts = sample(bayes_sir(Z), NUTS(1.0), 1000); 
+# Creates animation of ABM
+#sir_model = sir_initiation()
+#
+#abmvideo(
+#         "socialdist2.mp4",
+#         sir_model,
+#         sir_agent_step!,
+#         sir_model_step!;
+#         title = "Move",
+#         frames = 500,
+#         spf = 2,
+#         framerate = 25,
+#        )
